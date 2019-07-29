8d85f3da2cc87882a2ff7651cb1b226c8784ace2
Emil Bostijancic <emil.bostijancic@gmail.com>
2019-07-29 20:17:02 +0200

    initial commit.

---
+++ lib/rdtech_umc_logger.ex
@@ -0,0 +1,18 @@
+defmodule RdtechUmcLogger do
+  @moduledoc """
+  Documentation for RdtechUmcLogger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RdtechUmcLogger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rdtech_umc_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule RdtechUmcLoggerTest do
+  use ExUnit.Case
+  doctest RdtechUmcLogger
+
+  test "greets the world" do
+    assert RdtechUmcLogger.hello() == :world
+  end
+end
